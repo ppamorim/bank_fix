6f8e92995df6ca7c53fb549a6a1f4b0240b88689
Ciarán Ó hAoláin <ciaran@cohaolain.ie>
2020-05-30 01:27:07 +0100

    Generate a keystore if not done, use to sign apk

--- recompile.sh
+++ recompile.sh
@@ -10,9 +10,22 @@ fn=${1%.apk}
 rm -f $fn.unaligned.apk $fn.smali.apk
 rm -rf smali/build
 
+# We need a key to sign the apk (in order to install it easily)
+if [ ! -f "fix.keystore" ]; then
+    keytool -genkey -v -keystore fix.keystore \
+        -storepass fixerpass \
+        -dname "cn=Fixer, ou=Fixer, o=Fixer, c=NA" \
+        -alias fixerkey -keyalg RSA \
+        -keysize 2048 -validity 10000
+fi
+
 apktool b -f smali/ -o $fn.unaligned.apk
-jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore  -storepass android  $fn.unaligned.apk  androiddebugkey
-zipalign -v 4 $fn.unaligned.apk $fn.smali.apk
-rm -rf smali/build
+
+jarsigner -keystore fix.keystore \
+    -storepass fixerpass \
+    $fn.unaligned.apk fixerkey
+
+zipalign 4 $fn.unaligned.apk $fn.fixed.apk
+rm -rf smali/build $fn.unaligned.apk
 
 echo "Recompile done."
